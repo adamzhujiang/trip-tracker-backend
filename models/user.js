7d6b557c41bfdc0d8a12d0013ac270a9d56deb38
azhujiang <azhujiang15@gmail.com>
2025-09-03 19:31:26 -0700

    add mongodb connection

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    hashedPassword: {
+    password: {
         type: String,
         required: true
     }
