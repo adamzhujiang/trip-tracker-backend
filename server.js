fa8486468eb0d3f4e69f33e0fd59b2f2495c960f
spen <rtaylor4581@gmail.com>
2025-09-16 00:02:30 -0500

    fix server.js

--- server.js
+++ server.js
@@ -10,16 +10,27 @@ const userRouter = require("./controllers/user.js")
 const destinationRouter = require("./controllers/destination.js")
 const tripRouter = require("./controllers/trip.js")
 const attractionRouter = require("./controllers/attraction.js")
-
 mongoose.connect(process.env.MONGODB_URI);
+
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://trip-tracker-travel.netlify.app/', // replace with your actual Netlify URL
+  credentials: true
+}));
 app.use(express.json());
 app.use(logger('dev'));
+
+
+// Root route (health check)
+app.get("/", (req, res) => {
+  res.send("Trip Tracker Backend is live!");
+});  
+
 
 // Routes go here
 app.use('/auth', authRouter);
@@ -28,6 +39,23 @@ app.use('/trips', tripRouter)
 app.use("/trips/:tripId/destinations", destinationRouter)
 app.use("/destinations/:destinationId/attractions", attractionRouter)
 
-app.listen(3000, () => {
-  console.log(`The express app is on port ${3000}!`);
+
+
+const PORT = process.env.PORT || 3000; // use Heroku port or fallback to 3000 for local dev
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
